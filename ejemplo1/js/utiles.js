dfdd1f03bba3bf9efd168fb112235f4937f7e402
GenesisNUP30 <gushpiz998@g.educaand.es>
2025-11-12 08:28:38 +0100

    listarAlumnos hecho

--- ejemplo1/js/utiles.js
+++ ejemplo1/js/utiles.js
@@ -1,5 +1,32 @@
 function obtenerAlumnos() {
-    fetch("php/listaralumnos.php")
+    fetch("http://localhost/phpmyadmin/index.php?route=/sql&pos=0&db=instituto&table=alumnos")
         .then(response => response.json())
         .then(data => {
-            
+            alumnos = data;
+            mostrarAlumnos();
+        });
+}
+
+function mostrarAlumnos() {
+    let contenedor = document.getElementById("alumnos");
+    contenedor.innerHTML = "";
+
+    alumnos.forEach(alumno => {
+        let table = document.createElement("table");
+        table.className = "tabla-alumnos";
+        let tr = document.createElement("tr");
+        let td = document.createElement("td");
+        td.innerHTML = alumno.id;
+        tr.appendChild(td);
+        td.innerHTML = alumno.nombre;
+        tr.appendChild(td);
+        td = document.createElement("td");
+        td.innerHTML = alumno.apellidos;
+        tr.appendChild(td);
+        td = document.createElement("td");
+        td.innerHTML = alumno.nota;
+        tr.appendChild(td);
+        table.appendChild(tr);
+        contenedor.appendChild(table);
+    });
+}
